551a752ee2f3b0a83b9b07151d12e5dbe52c30b1
Yoosuf Ahamed <yahamed95@gmail.com>
2025-05-04 23:46:54 +0530

    sql code

--- src/main/webapp/scheme.sql
+++ src/main/webapp/scheme.sql
@@ -4,6 +4,7 @@ CREATE SCHEMA ⁠ OnlineStockManagementSystem ⁠ ;
 CREATE TABLE `Stock_Items` (
   `item_id` int NOT NULL AUTO_INCREMENT,
   `item_name` varchar(100) NOT NULL,
+  `item_model` varchar(100) DEFAULT NULL,
   `supplier_id` int DEFAULT NULL,
   `quantity` int DEFAULT '0',
   `unit_cost` decimal(10,2) DEFAULT NULL,
@@ -11,5 +12,6 @@ CREATE TABLE `Stock_Items` (
   `date_added` datetime NOT NULL,
   PRIMARY KEY (`item_id`)
 )
+
 
 
